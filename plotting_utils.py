4b98f7a66a53649abfaf8b60bf15e60f44c70740
abishekk <abishek85@gmail.com>
2020-01-28 10:17:17 -0500

    Modify seaborn style in matplotlib

---
+++ plotting_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Plotting: Utility Functions
+Stylized according to personal preferences
+
+Created on Mon Jan 27 12:39:06 2020
+
+@author: abishekk
+"""
+
+import matplotlib as mpl
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def Plotting(object):
+    """
+    Class for personalized plotting functions
+    """
+    
+    def __init__(self):
+        """
+        Initialize default values for font sizes etc
+        
+        Has n attributes: 
+        Returns
+        -------
+        None.
+
+        """
+        self.font_size = 14
+    
+    
